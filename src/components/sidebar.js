23e816f3eb2f6db0445631ea9495908fd7f88b9b
Joosep <joosep-elias.toru@voco.ee>
2023-03-13 15:14:35 +0200

    Kui kasutaja vajutab teaviku peale, näeb ta teaviku kohta infot

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -2,13 +2,11 @@ import react from "react";
 import styles from "../styles/Sidebar.module.css";
 
 const Sidebar = (props) => {
-    return (
-        <div className={styles.sidebar}>
-            {props.isOpen ?
-            <a className={styles.button}>AUTENDI</a>
-            : null}
-        </div>
-    );
-}
+  return (
+    <div className={styles.sidebar}>
+      {props.isOpen ? <a className={styles.button}>AUTENDI</a> : null}
+    </div>
+  );
+};
 
 export default Sidebar;
